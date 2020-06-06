718d42d0bbcb4378afa1bbbfdd7fcb0ea8a340a8
Petrovvich <alexromantrue@gmail.com>
2020-06-06 17:17:56 +0700

    added stack based on dynamically resized array

---
+++ src/main/java/ru/petrovich/algorithms/book/data/structures/stack/ResizingArrayStackOfStrings.java
@@ -0,0 +1,34 @@
+package ru.petrovich.algorithms.book.data.structures.stack;
+
+public class ResizingArrayStackOfStrings {
+    private String[] stringArray;
+    private int cursor = 0;
+
+    public ResizingArrayStackOfStrings() {
+        stringArray = new String[1];
+    }
+
+    public void push(String item) {
+        if (cursor == stringArray.length) {
+            resizeArray(2 * stringArray.length);
+        }
+        stringArray[cursor++] = item;
+    }
+
+    public String pop() {
+        String item = stringArray[--cursor];
+        stringArray[cursor] = null;
+        if (cursor > 0 && cursor == stringArray.length / 4) {
+            resizeArray(stringArray.length / 2);
+        }
+        return item;
+    }
+
+    private void resizeArray(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < cursor; i++) {
+            copy[i] = stringArray[i];
+        }
+        stringArray = copy;
+    }
+}
